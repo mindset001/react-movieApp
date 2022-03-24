d685fc154e8b51425484792c2b0b739d4773aaa9
Shittu-Moshood <shittumoshoodademola@gmail.com>
2022-03-24 13:17:34 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+import './App.scss';
+import Logo from './assets/MyTestApp.png'
+import axios from 'axios'
+import _ from 'lodash'
+
+function App() {
+
+  const [data, setdata] =  useState([])
+  const [search, setsearch] = useState('')
+
+  useEffect(async() => {
+      const movies = await axios.get(`http://www.omdbapi.com?i=tt3896198&apikey=7645a5c3&s=${search != '' ? search : 'movies'}`)
+      let groupData = _.groupBy(movies.data.Search, 'Type')
+      setdata(Object.entries(groupData))
+      
+  },[search])
+
+  return (
+    <div className="App">
+      <div className='app-bar'>
+          <img src={Logo} alt="logo" />
+        </div>
+      <div className='container'>
+        <div className='title'>
+           Watch something incredible.
+        </div>
+      </div>
+       <div className='body-content'>
+         <label htmlFor='search' >Search</label>
+         <input onChange={e => setsearch(e.target.value) }  id='search' />
+         
+         {
+           data.length && data.map((movie, index) => (
+             <div key={index} className="movie-container">
+               <div className='movie-title'>{ movie[0] }</div>
+                 <div className='layout'>
+                    { movie[1].map((item, index) => (
+                    <div key={index} className='img-container'>
+                    <img src={item.Poster} />
+                    <div className='movie-name'>{ item.Title }</div>
+                  </div>
+                  )) }
+                 </div>
+             </div>
+           ))
+         }
+       </div>
+    </div>
+  );
+}
+
+export default App;
